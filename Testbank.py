9d8e987d1f81d9f4455e29fc95b762b8058a30b5
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-05-14 19:33:18 -0400

    Update Testbank.py

--- Testbank.py
+++ Testbank.py
@@ -1,28 +1,28 @@
 import unittest
 
-from 567_bank import *
+from Bank import *
 
 class Testbank(unittest.TestCase):
 
     def testWithdrawHuey_success(self):
-        self.assertEqual(withdraw(Huey, 10), 'Right')
+        self.assertEqual(huey.withdraw(10), 'Right')
 
     def testWithdrawHuey_penalty(self):
-        self.assertEqual(withdraw(Huey, 100), 'Right')
+        self.assertEqual(huey.withdraw(100), 'Right')
 
     def testWithdrawDewey_success(self):
-        self.assertEqual(withdraw(Dewey, 5), 'Right')
+        self.assertEqual(dewey.withdraw(5), 'Right')
     
     def testWithdrawDewey_penalty(self):
-        self.assertEqual(withdraw(Dewey, 200), 'Right')
+        self.assertEqual(dewey.withdraw(200), 'Right')
     
     def testWithdrawLouie_success(self):
-        self.assertEqual(withdraw(Louie, 2) 'Right')
+        self.assertEqual(louie.withdraw(2) 'Right')
     
     def testWithdrawLouie_penalty(self):
-        self.assertEqual(withdraw(Louie, 20), 'Right')
-    
-
+        self.assertEqual(louie.withdraw(20), 'Right')
+   
     if __name__ == '__main__':
         print('Running unit tests')
         unittest.main()
+        

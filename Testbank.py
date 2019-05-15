03de53c6b4817efaf551421ee8843cb0a7cd8173
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-05-15 12:18:23 -0400

    Update Testbank.py

--- Testbank.py
+++ Testbank.py
@@ -10,6 +10,7 @@ class Testbank(unittest.TestCase):
         self.assertEqual(dewey.balance, 360)
         self.assertEqual(louie.balance, 35)
         self.assertEqual(scrooge.balance, 999980)
+        self.assertEqual(transaction, 1)
         
     def test2WithdrawHuey_penalty(self):   
         huey.withdraw(100)
@@ -17,6 +18,7 @@ class Testbank(unittest.TestCase):
         self.assertEqual(dewey.balance, 360)
         self.assertEqual(louie.balance, 35)
         self.assertEqual(scrooge.balance, 999985)
+        self.assertEqual(transaction, 2)
         
     def test3WithdrawDewey_success(self):
         dewey.withdraw(5)
@@ -24,6 +26,7 @@ class Testbank(unittest.TestCase):
         self.assertEqual(dewey.balance, 355)
         self.assertEqual(louie.balance, 40)
         self.assertEqual(scrooge.balance, 999975)
+        self.assertEqual(transaction, 3)
         
     def test4WithdrawDewey_penalty(self):
         dewey.withdraw(200)
@@ -31,6 +34,7 @@ class Testbank(unittest.TestCase):
         self.assertEqual(dewey.balance, 350)
         self.assertEqual(louie.balance, 40)
         self.assertEqual(scrooge.balance, 999980)
+        self.assertEqual(transaction, 4)
     
     def test5WithdrawLouie_success(self):
         louie.withdraw(2)
@@ -38,6 +42,7 @@ class Testbank(unittest.TestCase):
         self.assertEqual(dewey.balance, 352)
         self.assertEqual(louie.balance, 38)
         self.assertEqual(scrooge.balance, 999976)
+        self.assertEqual(transaction, 5)
        
     def test6WithdrawLouie_penalty(self):
         louie.withdraw(20)
@@ -45,8 +50,9 @@ class Testbank(unittest.TestCase):
         self.assertEqual(dewey.balance, 352)
         self.assertEqual(louie.balance, 33)
         self.assertEqual(scrooge.balance, 999981)
+        self.assertEqual(transaction, 6)
     
     if __name__ == '__main__':
         print('Running unit tests')
         unittest.main()
-        
+       

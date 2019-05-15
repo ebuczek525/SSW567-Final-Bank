f731dd1fc14babb0ec67919559db1a9e3bb9ef93
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-05-15 12:18:12 -0400

    Update Final_bank.py

--- Final_bank.py
+++ Final_bank.py
@@ -1,28 +1,41 @@
+transaction = int(0)
+
 class Bank:
     def __init__(self, name, accnum, balance):
         self.name = name
         self.accnum = accnum
         self.balance = balance
     def withdraw(self, amount):
+        global transaction
         if amount <= (0.1 * self.balance):
             if self.name == 'Huey':
                 self.balance -= amount
                 dewey.balance += amount
                 louie.balance += amount
                 scrooge.balance -= (2 * amount)
+                transaction += 1
             elif self.name == 'Dewey':
                 self.balance -= amount
                 huey.balance += amount
                 louie.balance += amount
                 scrooge.balance -= (2 * amount)
+                transaction += 1
             elif self.name == 'Louie':
                 self.balance -= amount
                 dewey.balance += amount
                 huey.balance += amount
                 scrooge.balance -= (2 * amount)
+                transaction += 1
         elif amount > (0.1 * self.balance):
             self.balance -= 5
             scrooge.balance += 5
+            transaction += 1
+            
+        print("Transaction number",transaction)
+        print("Huey's balance is",huey.balance)
+        print("Dewey's balance is",dewey.balance)
+        print("Louie's balance is",louie.balance)
+        print("Scrooge McDuck's balance is",scrooge.balance)
 
 huey = Bank('Huey', '700007', 150)
 dewey = Bank('Dewey', '800008', 350)
